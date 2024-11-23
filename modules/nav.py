8d868b4ef02449e62359e057952a7b98248ed7c6
vlftkak2 <vlftkak2@naver.com>
2024-11-23 20:51:53 +0900

    Update nav.py

--- modules/nav.py
+++ modules/nav.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 def sidebar():
-    with st.sidebar:
-        st.image("images\\APR.png")
+    with st.sidebar:ㄴ
+        #st.image("images\\APR.png")
         st.header('메뉴')
         st.page_link('pages/pdf.py', label='HKTVMALL 송장주문번호', icon='🌏')
         
