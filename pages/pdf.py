9d5cdfb3a3a1f4715a6dfc2a9a9a6347fe5afce7
vlftkak2 <vlftkak2@naver.com>
2024-11-23 20:42:00 +0900

    Update pdf.py

--- pages/pdf.py
+++ pages/pdf.py
@@ -6,6 +6,8 @@ from io import BytesIO
 from datetime import datetime
 from container.footer import footer
 from modules.nav import sidebar
+import spacy
+from spacy import displacy
 
 st.set_page_config(page_title="HKTVMALL 송장주문번호",
                 page_icon="🌏",
@@ -14,6 +16,13 @@ st.set_page_config(page_title="HKTVMALL 송장주문번호",
 vert_space = '<div style="padding: 20px 5px;"></div>'
 sidebar()
 footer()
+
+@st.cache_resource
+def load_model():
+    nlp = spacy.load("en_core_web_sm")
+    return nlp
+
+nlp = load_model()
 
 curdate = datetime.now()
 curdate = curdate.strftime("%Y-%m-%d")
